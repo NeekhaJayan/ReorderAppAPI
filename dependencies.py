bc5d7e85153ddc2ae5320ecb5d774a5f0c4242d1
Neekha <neekha@decagrowth.com>
2025-01-02 10:57:18 +0530

    Signed-off-by: Neekha <neekha@decagrowth.com>

---
+++ dependencies.py
@@ -0,0 +1,17 @@
+import boto3
+from passlib.context import CryptContext
+import os
+
+ 
+AWS_BUCKET='reorderpro.decagrowth.com'
+AWS_ACCESS_KEY_ID=os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY=os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_REGION_NAME='ap-south-1'
+
+s3_resource = boto3.resource('s3',region_name=AWS_REGION_NAME, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+BUCKET=s3_resource.Bucket(AWS_BUCKET)
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_s3_client():
+    return boto3.client('s3',region_name=AWS_REGION_NAME, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
